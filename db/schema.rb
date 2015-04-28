2bc4396b587ec8afa567b23c96bb4dcf1cedbcfe
Prelang Builder <builder@prelang.com>
2015-04-28 12:49:55 +0000

    Executed 'rake db:migrate' (development and test) to migrate all generated migrations which created schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150428124257) do
+ActiveRecord::Schema.define(version: 20150428124411) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -45,5 +45,104 @@ ActiveRecord::Schema.define(version: 20150428124257) do
 
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+
+  create_table "classrooms", force: true do |t|
+    t.string   "building"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "course_offerings", force: true do |t|
+    t.integer  "course_id"
+    t.integer  "section_number"
+    t.string   "year"
+    t.string   "semester"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "course_offerings", ["course_id"], name: "index_course_offerings_on_course_id"
+
+  create_table "course_offerings_instructors", force: true do |t|
+    t.integer "course_offering_id"
+    t.integer "instructor_id"
+  end
+
+  add_index "course_offerings_instructors", ["course_offering_id"], name: "index_course_offerings_instructors_on_course_offering_id"
+  add_index "course_offerings_instructors", ["instructor_id"], name: "index_course_offerings_instructors_on_instructor_id"
+
+  create_table "course_timings", force: true do |t|
+    t.integer  "timing_id"
+    t.integer  "course_offering_id"
+    t.integer  "classroom_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "course_timings", ["classroom_id"], name: "index_course_timings_on_classroom_id"
+  add_index "course_timings", ["course_offering_id"], name: "index_course_timings_on_course_offering_id"
+  add_index "course_timings", ["timing_id"], name: "index_course_timings_on_timing_id"
+
+  create_table "courses", force: true do |t|
+    t.string   "title"
+    t.string   "credits"
+    t.text     "syllabus"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "departments", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "enrollments", force: true do |t|
+    t.string   "grade"
+    t.integer  "student_id"
+    t.integer  "course_offering_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "enrollments", ["course_offering_id"], name: "index_enrollments_on_course_offering_id"
+  add_index "enrollments", ["student_id"], name: "index_enrollments_on_student_id"
+
+  create_table "instructors", force: true do |t|
+    t.string   "name"
+    t.string   "title"
+    t.integer  "department_id"
+    t.integer  "identification_number"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "instructors", ["department_id"], name: "index_instructors_on_department_id"
+
+  create_table "prerequisites", force: true do |t|
+    t.integer  "prerequisite_id"
+    t.integer  "course_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "prerequisites", ["course_id"], name: "index_prerequisites_on_course_id"
+
+  create_table "students", force: true do |t|
+    t.string   "name"
+    t.string   "program"
+    t.string   "student_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "timings", force: true do |t|
+    t.time     "start"
+    t.time     "end"
+    t.string   "day"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
