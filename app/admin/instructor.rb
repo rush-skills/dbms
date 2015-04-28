a0407839f8444075b1bc978e769443fea27df310
Prelang Builder <builder@prelang.com>
2015-04-28 12:44:53 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Instructor' to allow for create, update

--- app/admin/instructor.rb
+++ app/admin/instructor.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Instructor do
+  permit_params :name, :title, :identification_number
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
