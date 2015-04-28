17038a28a5f84de4a54e7ebfc4cae1afd250df40
Prelang Builder <builder@prelang.com>
2015-04-28 12:45:53 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Prerequisite' to allow for create, update

--- app/admin/prerequisite.rb
+++ app/admin/prerequisite.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Prerequisite do
+  permit_params :prerequisite_id
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
