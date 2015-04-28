773e8d39f293019ece21954e9cd8fe3f7a80d1a5
Prelang Builder <builder@prelang.com>
2015-04-28 12:45:24 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Timing' to allow for create, update

--- app/admin/timing.rb
+++ app/admin/timing.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Timing do
+  permit_params :start, :end, :day
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
